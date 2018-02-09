a01c46e8f89a4b2220136cf1ff172f4cf62c71b7
Tornike Enukidze <enuka15@outlook.com>
2018-02-09 10:31:23 +0400

    Renamed and added some comment in Script

--- psScript.ps1
+++
@@ -1,4 +0,0 @@
-Get-Counter -Counter "\processor(_total)\% processor time" , "\memory\available mbytes" , 
-"\LogicalDisk(c:)\Free Megabytes" -SampleInterval 600 -MaxSamples 54 -ComputerName "10.38.0.231","10.38.0.232", "10.38.0.233", "10.38.0.236", "10.38.0.237","10.38.0.226","10.38.0.225","10.38.0.46","10.38.0.241","10.38.0.243","10.38.0.245","10.38.0.249","10.38.0.221","10.38.0.17","10.38.0.191", "10.38.0.130","10.38.0.131","10.38.0.218" | Export-Counter -Path d:\RDS.csv -FileFormat CSV -Force
-   
-
